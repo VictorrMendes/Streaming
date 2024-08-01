e7682997198d74b0c8ba00881e9c6ba1aba562de
VictorrMendes <victo.mendes.souza@gmail.com>
2024-07-31 22:10:06 -0300

    responsividade

--- script/login.js
+++ script/login.js
@@ -1,19 +1,29 @@
 const login = document.querySelector('.submit');
 
-login.addEventListener('click',verificarAcesso);
+alert('Olá, bem vindo ao site, o login e a senha é "admin" ');
 
-function verificarAcesso(){
+login.addEventListener('click', verificarAcesso);
 
-var email = document.querySelector('#email').value;
-var password = document.querySelector('#password').value;
+function verificarAcesso() {
 
-const login_acesso = "admin";
-const senha_acesso = "admin";
+    var email = document.querySelector('#email').value;
+    var password = document.querySelector('#password').value;
 
-if (email === login_acesso && password === senha_acesso) {
-    window.open('./documents/perfis.html')
-    alert('Bem vinda!');
-} else {
-    alert('Senha ou Login errada bb!');
-};
+    fetch('credenciais.json')
+        .then(response => response.json()
+            .then(data => {
+                const login_acesso = data.KEY_LOGIN;
+                const senha_acesso = data.KEY_PASSWORD;
+
+
+
+
+
+                if (email === login_acesso && password === senha_acesso) {
+                    window.open('./documents/perfis.html')
+                    alert('Bem vinda!');
+                } else {
+                    alert('Senha ou Login errada bb!');
+                };
+            }));
 }
